60369a1f43a7f744c39fe6e12b8445373d429926
mhh <mike.hukiewitz@robotter.ai>
2023-10-15 20:26:22 +0200

    Remove unused imports and commented code in Prompts.tsx.

--- src/shared/components/Prompts.tsx
+++ src/shared/components/Prompts.tsx
@@ -3,19 +3,10 @@ import TextInput from '@components/form/TextInput';
 import AppModal from '@components/ui/AppModal';
 import Button from '@components/ui/Button';
 import ClickToCopy from '@shared/components/ClickToCopy';
-import useAuth from '@shared/hooks/useAuth';
 import useModal from '@shared/hooks/useModal';
-import useSelectData from '@shared/hooks/useSelectData';
-import { useAppDispatch, useAppSelector } from '@shared/hooks/useStore';
-import {
-  changeExecutionDetails,
-  postExecutionRequest,
-  resetExecutionDetails,
-} from '@slices/executionSlice';
-import { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { IoCheckbox } from 'react-icons/io5';
 import { useNavigate, useSearchParams } from 'react-router-dom';
-import { FadeLoader } from 'react-spinners';
 
 interface IDeletePrompt {
   title?: string;
@@ -80,151 +71,6 @@ export const DeletePrompt: React.FC<IDeletePrompt> = ({
     </>
   );
 };
-
-// export const ExecutePrompt: React.FC<{
-//   selectedHash: string;
-//   against: 'data' | 'algorithm';
-//   disabled?: boolean;
-//   btnStyle?: 'solid' | 'thin';
-//   btnSize?: 'sm' | 'md';
-// }> = ({ disabled, against, selectedHash, btnStyle, btnSize = 'sm' }) => {
-//   const dispatch = useAppDispatch();
-//   const auth = useAuth();
-//   const { details, success, isLoading, result } = useAppSelector(
-//     (state) => state.execution
-//   );
-//   const { isOpen, handleOpen, handleClose } = useModal();
-//   const { isSelect, handleNavigateWithSelect } = useSelectData();
-//   const [isExecuted, setIsExecuted] = useState(false);
-
-//   useEffect(() => {
-//     dispatch(
-//       changeExecutionDetails({
-//         input: 'owner',
-//         value: auth?.address,
-//       })
-//     );
-//     if (success) {
-//       dispatch(resetExecutionDetails());
-//     }
-//   }, [success]);
-
-//   const handleSelect = () => {
-//     handleNavigateWithSelect(against === 'data' ? '/data' : '/algorithms');
-//   };
-
-//   return (
-//     <>
-//       {btnStyle === 'solid' ? (
-//         <Button
-//           text={isSelect ? 'Select' : 'Use'}
-//           size="lg"
-//           onClick={() => {
-//             dispatch(
-//               changeExecutionDetails({
-//                 input: against === 'data' ? 'algorithmID' : 'datasetID',
-//                 value: selectedHash,
-//               })
-//             );
-//             handleOpen();
-//           }}
-//           disabled={disabled}
-//         />
-//       ) : (
-//         <div className="w-full flex justify-end">
-//           <Button
-//             text={isSelect ? 'Select' : 'Use'}
-//             size={btnSize}
-//             btnStyle="outline-primary"
-//             onClick={() => {
-//               dispatch(
-//                 changeExecutionDetails({
-//                   input: against === 'data' ? 'algorithmID' : 'datasetID',
-//                   value: selectedHash,
-//                 })
-//               );
-//               handleOpen();
-//             }}
-//             disabled={disabled}
-//           />
-//         </div>
-//       )}
-//       <AppModal
-//         title={isExecuted ? 'Result' : 'Execute'}
-//         isOpen={isOpen}
-//         handleClose={handleClose}
-//         withHeader={!isLoading}
-//         fullWidth={isExecuted}
-//       >
-//         {isLoading ? (
-//           <div className="flex flex-col items-center gap-4">
-//             <h1>Pending execution</h1>
-//             <div className="p-4">
-//               <FadeLoader color="#1DC3CF" height={15} margin={-2.5} width={3} />
-//             </div>
-//             <p>Please wait</p>
-//           </div>
-//         ) : null}
-//         {isExecuted ? (
-//           <div>
-//             <div className="bg-[#f3f3f3] w-full min-h-[40px] rounded mt-4 relative p-4 overflow-auto">
-//               <pre>{JSON.stringify({ ...result }, null, 2)}</pre>
-//             </div>
-//             <div className="flex justify-center mt-5">
-//               <Button text="Okay" size="lg" onClick={handleClose} />
-//             </div>
-//           </div>
-//         ) : null}
-//         {!isLoading && !isExecuted ? (
-//           <>
-//             <div className="mb-[25px] flex flex-col gap-5">
-//               <TextInput
-//                 label={`Paste a hash of the ${against}, that will be applied to the ${
-//                   against === 'data' ? 'algorithm' : 'data'
-//                 }`}
-//                 placeholder={`Hash of ${against}`}
-//                 bgColor="#F6F8FB"
-//                 value={
-//                   against === 'data' ? details.datasetID : details.algorithmID
-//                 }
-//                 onChange={(e) =>
-//                   dispatch(
-//                     changeExecutionDetails({
-//                       input: against === 'data' ? 'datasetID' : 'algorithmID',
-//                       value: e.target.value,
-//                     })
-//                   )
-//                 }
-//                 fullWidth
-//               />
-//             </div>
-//             <div className="flex flex-col gap-4">
-//               <Button
-//                 text={`Select ${against}`}
-//                 btnStyle="outline-primary"
-//                 size="lg"
-//                 fullWidth
-//                 onClick={handleSelect}
-//                 withoutBorder
-//               />
-//               <Button
-//                 text="Apply"
-//                 size="lg"
-//                 isLoading={isLoading}
-//                 onClick={() =>
-//                   dispatch(postExecutionRequest(details)).then(() =>
-//                     setIsExecuted(true)
-//                   )
-//                 }
-//                 fullWidth
-//               />
-//             </div>
-//           </>
-//         ) : null}
-//       </AppModal>
-//     </>
-//   );
-// };
 
 export const PublishedModal: React.FC<{
   title: string;
