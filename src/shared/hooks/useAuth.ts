8c0cea871e7e582f4afce756a9b48218f36756e1
Riki <ricardocr987@gmail.com>
2024-06-26 15:54:04 +0200

    fix: navigate to data only when root

--- src/shared/hooks/useAuth.ts
+++ src/shared/hooks/useAuth.ts
@@ -1,12 +1,12 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useWallet } from '@solana/wallet-adapter-react';
-import { useNavigate } from 'react-router-dom';
-import Cookies from 'universal-cookie';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { WalletName } from '@solana/wallet-adapter-base';
 import { useAuth } from '@contexts/auth-provider';
 
 const useWalletAuth = () => {
   const navigate = useNavigate();
+  const location = useLocation();
   const { disconnect, select } = useWallet();
   const [isLoading, setIsLoading] = useState(false);
   const { tokenValidated, removeToken } = useAuth();
@@ -14,7 +14,7 @@ const useWalletAuth = () => {
   useEffect(() => {
     if (tokenValidated) {
       setIsLoading(false);
-      navigate('/data', { replace: true });
+      if (location.pathname === '/') navigate('/data', { replace: true });
     } else {
       navigate('/', { replace: true });
     }
